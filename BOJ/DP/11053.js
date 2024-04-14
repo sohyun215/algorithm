d39aff8f9784a406a0ddc5413e933a7771879a77
sohyun <kshyun0215@gmail.com>
2024-04-14 17:58:37 +0900

    BOJ - 11053 가장 긴 증가하는 부분 수열

---
+++ BOJ/DP/11053.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const dp = Array(n).fill(1);
+
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+console.log(Math.max(...dp));
